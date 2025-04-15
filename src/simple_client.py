9fba3bff4a336a07c27b29965c895190b9523bb3
Rui Barbosa <barduinor@gmail.com>
2025-04-15 14:44:13 -0400

    fix ai memory

--- src/simple_client.py
+++ src/simple_client.py
@@ -1,7 +1,10 @@
 # Create server parameters for stdio connection
+import uuid
+
 from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
@@ -30,6 +33,9 @@ async def main():
             "src/mcp_server_box.py",
         ],
     )
+    memory = MemorySaver()
+    chat_id = uuid.uuid4()
+    config = {"configurable": {"thread_id": str(chat_id)}}
 
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
@@ -40,20 +46,23 @@ async def main():
             tools = await load_mcp_tools(session)
 
             # Create and run the agent
-            agent = create_react_agent(model, tools)
+            agent = create_react_agent(model, tools, checkpointer=memory)
 
             # loop through user input
             # print("Enter 'exit' to quit.")
+            history_index = 0
             while True:
                 user_input = prompt_user("Prompt (exit to quit)")
                 if user_input.lower() == "exit":
                     break
 
                 # Send the user input to the agent
-                agent_response = await agent.ainvoke({"messages": user_input})
+                agent_response = await agent.ainvoke(
+                    {"messages": user_input}, config=config
+                )
                 # print(agent_response)
                 messages = agent_response.get("messages", [])
-                for message in messages:
+                for message in messages[history_index:]:
                     # use different prints depending on message object type
                     if isinstance(message, HumanMessage):
                         print_human_message(message)
@@ -65,7 +74,9 @@ async def main():
                         print_markdown(f"**Unknown message type:** {type(message)}")
                     # print(type(message))
                     # print_markdown(message.content)
+                    history_index += 1
                     print_markdown("---")
+                # history_index = len(messages - 1)
 
 
 if __name__ == "__main__":
